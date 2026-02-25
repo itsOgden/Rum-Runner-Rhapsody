4faa0db6ffda956d03041e03ac6be3aca22f33ac
Phil <pogden215@gmail.com>
2026-02-25 12:12:21 -0500

    Cleanup and set titles of buttons

--- packages/rrr-streamdeck/src/actions/play-sound.ts
+++ packages/rrr-streamdeck/src/actions/play-sound.ts
@@ -1,4 +1,4 @@
-import streamDeck, { action, KeyDownEvent, PropertyInspectorDidAppearEvent, SendToPluginEvent, SingletonAction } from "@elgato/streamdeck";
+import streamDeck, { action, DidReceiveSettingsEvent, KeyDownEvent, PropertyInspectorDidAppearEvent, SendToPluginEvent, SingletonAction, WillAppearEvent } from "@elgato/streamdeck";
 import { rrrClient } from "../rrr-client";
 
 type PlaySoundSettings = {
@@ -24,6 +24,16 @@ rrrClient.on("message", (data: { type: string; sounds?: SoundItem[]; folderSelec
 
 @action({ UUID: "com.pdog1.rum-runner-rhapsody.playsound" })
 export class PlaySound extends SingletonAction<PlaySoundSettings> {
+	override async onWillAppear(ev: WillAppearEvent<PlaySoundSettings>): Promise<void> {
+		const { soundName, soundKey } = ev.payload.settings;
+		await ev.action.setTitle(soundName || soundKey || "");
+	}
+
+	override async onDidReceiveSettings(ev: DidReceiveSettingsEvent<PlaySoundSettings>): Promise<void> {
+		const { soundName, soundKey } = ev.payload.settings;
+		await ev.action.setTitle(soundName || soundKey || "");
+	}
+
 	override onKeyDown(ev: KeyDownEvent<PlaySoundSettings>): void {
 		const { soundKey } = ev.payload.settings;
 		if (soundKey) {
@@ -39,13 +49,21 @@ export class PlaySound extends SingletonAction<PlaySoundSettings> {
 		});
 	}
 
-	override onSendToPlugin(ev: SendToPluginEvent<{ type: string }, PlaySoundSettings>): void {
+	override async onSendToPlugin(ev: SendToPluginEvent<{ type: string }, PlaySoundSettings>): Promise<void> {
 		if (ev.payload.type === "requestSounds") {
-			streamDeck.ui.sendToPropertyInspector({
+			await streamDeck.ui.sendToPropertyInspector({
 				type: "soundsList",
 				sounds: currentSounds,
 				folderSelected,
 			});
+			const settings = await ev.action.getSettings();
+			const title = settings.soundName || settings.soundKey || "";
+			if (title) {
+				await ev.action.setTitle(title);
+				await ev.action.setSettings({
+
+				})
+			}
 		}
 	}
 }
--- packages/rrr-streamdeck/src/plugin.ts
+++ packages/rrr-streamdeck/src/plugin.ts
@@ -1,19 +1,14 @@
 import streamDeck from "@elgato/streamdeck";
 
-import { IncrementCounter } from "./actions/increment-counter";
 import { PlaySound } from "./actions/play-sound";
 import { StopAll } from "./actions/stop-all";
 import { rrrClient } from "./rrr-client";
 
-// We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel("trace");
 
-// Register actions.
-streamDeck.actions.registerAction(new IncrementCounter());
 streamDeck.actions.registerAction(new PlaySound());
 streamDeck.actions.registerAction(new StopAll());
 
-// Finally, connect to the Stream Deck.
 streamDeck.connect();
 
 rrrClient.on("connected", () => {
